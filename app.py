43d0e1cff2112f287c8cd6c830bf0d40e5a0ea19
Ben <50188950+ShavedWhale1@users.noreply.github.com>
2022-10-10 14:55:35 -0400

    Working localhost application!

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 # Ben Siri 10-10-22
 # Learn flask
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello world!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
